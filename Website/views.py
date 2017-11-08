28041bc0cd5f891552ec42911600288c2a12b1ee
Abhay <abhaytyagi1998@gmail.com>
2017-11-08 18:40:52 +0530

    UI Improvements

--- Website/views.py
+++ Website/views.py
@@ -19,16 +19,16 @@ def index(request):
 
 def play_it(request):
 	os.chdir("/home/mozart/char-rnn")
-	print("i am here")
 	os.system("ls")
-	#os.system("cd 'char-rnn'")
 	num = randint(0, 5000)
 	print ("Random generator: " + str(num))
 	os.system("cp output.mid /home/mozart/Website/media/output.mid")
 	os.system("th shiz.lua -seed " + str(num) + " cv/lm_lstm_epoch33.77_0.7266.t7")
 	os.system("ruby txt_to_midi.rb out.txt")
-	os.chdir("/home/mozart/Website")	
-
+	os.chdir("/home/mozart/Website")
+	
+	command = "timidity /media/output.mid -Ow /media/outfile.mp3"
+	os.system(command)
 	#return render(request, 'Website/play_it.html', {})
 	# return render(request, 'Website/play_it.html', {'urll': urll}
 	return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
