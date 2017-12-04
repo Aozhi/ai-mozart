5b452b3968fba594539e98d5ef59327df6b15ef3
Saurabh <samzor999@live.com>
2017-12-04 09:45:33 +0000

    Migrated to Google Cloud

--- Website/views.py
+++ Website/views.py
@@ -16,14 +16,14 @@ def index(request):
 	return render(request, 'Website/index.html', {})
 
 def play_it(request):
-	os.chdir("/home/mozart/char-rnn")
+	os.chdir("/home/bsaurabh1/char-rnn")
 	os.system("ls")
 	num = randint(0, 5000)
 	print ("Random generator: " + str(num))
-	os.system("cp output.mid /home/mozart/Website/media/output.mid")
+	os.system("cp output.mid /home/bsaurabh1/Website/media/output.mid")
 	os.system("th shiz.lua -seed " + str(num) + " cv/lm_lstm_epoch33.77_0.7266.t7")
 	os.system("ruby txt_to_midi.rb out.txt")
-	os.chdir("/home/mozart/Website")
+	os.chdir("/home/bsaurabh1/Website")
 
 	command = "timidity media/output.mid -Ow -o outfile.mp3"
 	os.system(command)
