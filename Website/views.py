d8e6987463dcddb0b3df0d82117d604ec82c3cce
Ubuntu <mozart@mozart.4dv5wd1bt4ce5apjlwjhxups5c.bx.internal.cloudapp.net>
2018-03-05 08:09:45 +0000

    Migrate

--- Website/views.py
+++ Website/views.py
@@ -16,14 +16,14 @@ def index(request):
 	return render(request, 'Website/index.html', {})
 
 def play_it_happy(request):
-	os.chdir("/home/saurabhbodhe/char-rnn")
+	os.chdir("/home/mozart/char-rnn")
 	os.system("ls")
 	num = randint(0, 5000)
 	print ("Random generator: " + str(num))
-	os.system("th shiz.lua -seed " + str(num) + " cv/lm_lstm_epoch50.00_0.9914.t7")
+	os.system("th shiz.lua -seed " + str(num) + " cv/lm_lstm_epoch50.00_0.7340.t7")
 	os.system("ruby txt_to_midi.rb out.txt")
-	os.system("cp output.mid /home/saurabhbodhe/ai-mozart/media/output.mid")
-	os.chdir("/home/saurabhbodhe/ai-mozart")
+	os.system("cp output.mid /home/mozart/ai-mozart/media/output.mid")
+	os.chdir("/home/mozart/ai-mozart")
 
 	command = "timidity -T 70 media/output.mid -Ow -o media/outfile.mp3"
 	os.system(command)
@@ -31,14 +31,14 @@ def play_it_happy(request):
 	return HttpResponseRedirect(request.META.get('HTTP_REFERER'))
 
 def play_it_sad(request):
-	os.chdir("/home/saurabhbodhe/char-rnn2")
+	os.chdir("/home/mozart/char-rnn")
 	os.system("ls")
 	num = randint(0, 5000)
 	print ("Random generator: " + str(num))
 	os.system("th shiz.lua -seed " + str(num) + " cv/lm_lstm_epoch20.07_0.6988.t7")
 	os.system("ruby txt_to_midi.rb out.txt")
-	os.system("cp output.mid /home/saurabhbodhe/ai-mozart/media/output.mid")
-	os.chdir("/home/saurabhbodhe/ai-mozart")
+	os.system("cp output.mid /home/mozart/ai-mozart/media/output.mid")
+	os.chdir("/home/mozart/ai-mozart")
 
 	command = "timidity -T 30 media/output.mid -Ow -o media/outfile.mp3"
 	os.system(command)
